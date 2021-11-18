58ffbc92b5811b97df6b7c5151519c908f64af9a
Leonard Ikeh <leoikeh99@gmail.com>
2021-11-18 14:18:45 +0100

    created Get Leaderboard route

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,13 @@
 import { injectable, inject } from 'inversify';
 import UserRepository from '../../repository/user.respository';
 import { systemResponse } from '../../../utils/response';
+import { Encrypt } from '../auth/encrpty.service';
+
+type user = {
+    email: string;
+    password: string;
+    username: string;
+};
 
 @injectable()
 export default class UserService {
@@ -20,7 +27,79 @@ export default class UserService {
         return systemResponse(true, '', user);
     }
 
-    public async updateUser(id: string): Promise<any> {
-        return systemResponse(true, '', {});
+    public async updateUser(body: user, id: string): Promise<any> {
+        const { username, email, password } = body;
+        const update: any = {};
+
+        const user = await this._userRepository.findById(id);
+        if (!user) {
+            return systemResponse(false, 'Invalid user', {});
+        }
+
+        if (username) {
+            const checkUsername = await this._userRepository.findOne({ $and: [{ username }, { _id: { $not: { $eq: user.id } } }] });
+            if (checkUsername) {
+                return systemResponse(false, 'Username already exists', {});
+            }
+            update.username = username;
+        }
+
+        if (email) {
+            const checkEmail = await this._userRepository.findOne({ $and: [{ email }, { _id: { $not: { $eq: user.id } } }] });
+            if (checkEmail) {
+                return systemResponse(false, 'Email already exists', {});
+            }
+            update.email = email;
+        }
+
+        if (password) {
+            const _password = Encrypt(password);
+            update.password = _password;
+        }
+
+        const updateUser = await this._userRepository.updateById(user.id, update);
+        if (!updateUser) {
+            return systemResponse(false, 'Error while updating user', {});
+        }
+
+        const getUser = await this._userRepository.findById(user.id, '-password');
+        if (!updateUser) {
+            return systemResponse(false, 'Error in rettrieving user data', {});
+        }
+
+        return systemResponse(true, 'Update Successfull', getUser);
+    }
+
+    public async getLeaderBoard(query: any): Promise<any> {
+        let { sortBy, page, limit } = query;
+
+        const filters: string[] = ['earnings', 'points', 'gold', 'silver', 'bronze'];
+        if (filters.some((filter) => sortBy !== filter)) sortBy === 'earnings';
+
+        const sort: any = {};
+        if (sortBy === 'earnings') sort.earnings = -1;
+        if (sortBy === 'points') sort.points = -1;
+        if (sortBy === 'gold') sort.gold = -1;
+        if (sortBy === 'silver') sort.silver = -1;
+        if (sortBy === 'bronze') sort.bronze = -1;
+
+        if (!page) page = 1;
+        if (!limit) limit = 2;
+
+        page = parseInt(page);
+        limit = parseInt(limit);
+        const skip = (page - 1) * limit;
+
+        const leaderBoard = await this._userRepository.findWithOptions({}, { limit, skip, sort });
+        const nextPage = await this._userRepository.findWithOptions({}, { limit, skip: skip + limit, sort });
+
+        const results = {
+            nextPage: nextPage.length !== 0 ? true : false,
+            page,
+            limit,
+            sortBy,
+            leaderBoard
+        };
+        return systemResponse(true, 'Get LeaderBoard', results);
     }
 }
